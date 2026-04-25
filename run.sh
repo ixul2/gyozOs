57ede39de2b0cfa9e55e17089655ca62616e8d6f
Lucas Hermier--Gastineau <lhermiergastinea@clipper.ens.fr>
2026-04-25 16:33:54 +0200

    three times the charm

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 as -o bootentry.o  bootentry.S
-gcc -ffreestanding -c -no-pie boot.c -o boot.o
-ld -o bootentry.bin --oformat=binary -Ttext 0x7c00 -e main bootentry.o
-qemu-system-x86_64 bootentry.bin
+gcc -ffreestanding -c -fno-pie boot.c -o boot.o
+ld -o bootentry.bin --oformat=binary -Ttext 0x7c00 -e boot_start bootentry.o boot.o
+qemu-system-x86_64 -d in_asm,cpu,int,exec -D debug-log -drive format=raw,file=bootentry.bin
