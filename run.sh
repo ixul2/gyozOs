40ebac1cb7bfdfbdfd4eea2065ee6fffaa9363d9
Lucas Hermier--Gastineau <lhermiergastinea@clipper.ens.fr>
2026-04-25 17:38:49 +0200

    cleaned up

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
-as -o bootentry.o  bootentry.S
-gcc -ffreestanding -c -fno-pie boot.c -o boot.o
-ld -o bootentry.bin --oformat=binary -T boot.ld -e boot_start bootentry.o boot.o
-gcc -o boot_maker mkbootdisk.c
-./boot_maker bootentry.bin > bootable_disk
-qemu-system-x86_64 -d in_asm,cpu,int,exec -D debug-log -drive format=raw,file=bootable_disk
+as -o obj/bootentry.o  bootentry.S
+gcc -ffreestanding -c -fno-pie boot.c -o obj/boot.o
+ld -o obj/bootentry.bin --oformat=binary -T build/boot.ld -e boot_start obj/bootentry.o obj/boot.o
+gcc -o obj/boot_maker build/mkbootdisk.c
+obj/boot_maker obj/bootentry.bin > bootable_disk
+#qemu-system-x86_64 -d in_asm,cpu,int,exec -D debug-log -drive format=raw,file=bootable_disk
+qemu-system-x86_64 -drive format=raw,file=bootable_disk
