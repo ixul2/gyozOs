50e2e04fdec254c0a1bdccd2072e618e97dcc834
Lucas Hermier--Gastineau <lhermiergastinea@clipper.ens.fr>
2026-04-25 17:30:30 +0200

    WORKING BOOTLOADER

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 as -o bootentry.o  bootentry.S
 gcc -ffreestanding -c -fno-pie boot.c -o boot.o
-ld -o bootentry.bin --oformat=binary -Ttext 0x7c00 -e boot_start bootentry.o boot.o
-qemu-system-x86_64 -d in_asm,cpu,int,exec -D debug-log -drive format=raw,file=bootentry.bin
+ld -o bootentry.bin --oformat=binary -T boot.ld -e boot_start bootentry.o boot.o
+gcc -o boot_maker mkbootdisk.c
+./boot_maker bootentry.bin > bootable_disk
+qemu-system-x86_64 -d in_asm,cpu,int,exec -D debug-log -drive format=raw,file=bootable_disk
