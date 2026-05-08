1f842a2f03d4def65cffb50866c34f3470cbc4b5
Lucas Hermier--Gastineau <lhermiergastinea@clipper.ens.fr>
2026-05-08 02:36:37 +0200

    working version

--- run.sh
+++
@@ -1,9 +0,0 @@
-as -o obj/bootentry.o  bootentry.S
-gcc -Os -ffreestanding -c -fno-inline -fomit-frame-pointer boot.c -o obj/boot.o
-ld -o obj/bootentry.bin --oformat=binary -T build/boot.ld -e boot_start obj/bootentry.o obj/boot.o
-gcc -o obj/boot_maker build/mkbootdisk.c
-gcc -ffreestanding -c kernel.c -o obj/kernel.o
-ld -o obj/kernel_final.o -e kernel obj/kernel.o
-obj/boot_maker obj/bootentry.bin obj/kernel_final.o > bootable_disk
-#qemu-system-x86_64 -d in_asm,cpu,int,exec -D debug-log -drive format=raw,file=bootable_disk -no-reboot
-qemu-system-x86_64 -drive format=raw,file=bootable_disk -no-reboot 
