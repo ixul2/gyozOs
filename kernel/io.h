7995a47b110aab7c4c6a93996ab92fdecd8eeded
Alixe <hugo.dethorey@ens.psl.eu>
2026-05-09 09:54:23 +0200

    virtual memory started

--- kernel/io.h
+++ kernel/io.h
@@ -3,5 +3,12 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#define PAGE_DIRECTORY_SIZE 1024
+#define PAGE_TABLE_SIZE 1024
+#define PAGE_SIZE 4096
+#define PAGE_PRESENT 0x1
+#define PAGE_RW      0x2
+#define PAGE_USER    0x4
+
 void console_print(uint32_t l, uint32_t c, const char* s);
 char keyboard_to_ascii(unsigned char c, int is_shifted);
--- kernel/virtual_memory.c
+++ kernel/virtual_memory.c
@@ -1,2 +1,15 @@
 #include "virtual_memory.h"
 #include "io.h"
+
+void memory_init(void){
+    uint32_t page_directory[PAGE_DIRECTORY_SIZE] __attribute__ ((aligned(PAGE_SIZE)));
+    for(uint32_t i = 0; i<PAGE_DIRECTORY_SIZE; i++){
+        page_directory[i] = 0x00000002;
+    }
+    uint32_t first_page_table[PAGE_TABLE_SIZE] __attribute__ ((aligned(PAGE_SIZE)));
+    for(uint32_t i = 0; i<PAGE_TABLE_SIZE; i++){
+        first_page_table[i] = (i * PAGE_SIZE) | PAGE_PRESENT | PAGE_RW;
+    }
+    page_directory[0] =
+        ((uint32_t)first_page_table) | PAGE_PRESENT | PAGE_RW;
+}
