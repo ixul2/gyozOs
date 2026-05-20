505b031abb0ec2909e6e30f9ddcb16a7702b760b
Alixe <hugo.dethorey@ens.psl.eu>
2026-05-20 12:35:58 +0200

    reordered process

--- kernel/process.c
+++ kernel/process.c
@@ -8,7 +8,7 @@ extern uint8_t _binary_userspace__obj_p_nano_bin_end[];
 extern uint8_t _binary_userspace__obj_p_idle_bin_start[];
 extern uint8_t _binary_userspace__obj_p_idle_bin_end[];
 
-static proc procs[PROC_NUMBER + 1];
+static proc procs[PROC_NUMBER];
 proc* current;
 
 typedef struct ramimage {
@@ -17,12 +17,12 @@ typedef struct ramimage {
 } ramimage;
 
 ramimage ramimages[] = {
+    {   .begin = _binary_userspace__obj_p_idle_bin_start,
+        .end = _binary_userspace__obj_p_idle_bin_end },
     {   .begin = _binary_userspace__obj_p_shell_bin_start,
         .end = _binary_userspace__obj_p_shell_bin_end },
     {   .begin = _binary_userspace__obj_p_nano_bin_start,
-        .end = _binary_userspace__obj_p_nano_bin_end },
-    {   .begin = _binary_userspace__obj_p_idle_bin_start,
-        .end = _binary_userspace__obj_p_idle_bin_end },
+        .end = _binary_userspace__obj_p_nano_bin_end }
 };
 
 void init_process(proc* p){
@@ -48,10 +48,11 @@ void load_process(proc* p, int program) {
 
     for (size_t i = 0; i < pages_needed; i++) {
         map_page(p->page_table,
-                 user_base + i * PAGE_SIZE,
-                 user_base + i * PAGE_SIZE,
-                 PTE_P | PTE_W | PTE_U,
-                 NULL);
+            user_base + i * PAGE_SIZE,
+            user_base + i * PAGE_SIZE,
+            PTE_P | PTE_W | PTE_U,
+            NULL);
+        assign_physical_page(user_base + i * PAGE_SIZE,p->id);
     }
 
     memcpy((void*)user_base, prog.begin, bin_size);
@@ -63,10 +64,11 @@ void load_process(proc* p, int program) {
     size_t stack_pages = 1;
     for (size_t i = 0; i < stack_pages; i++) {
         map_page(p->page_table,
-                user_base + PROC_SIZE - (i + 1) * PAGE_SIZE,
-                user_base + PROC_SIZE - (i + 1) * PAGE_SIZE,
-                PTE_P | PTE_W | PTE_U,
-                NULL);
+            user_base + PROC_SIZE - (i + 1) * PAGE_SIZE,
+            user_base + PROC_SIZE - (i + 1) * PAGE_SIZE,
+            PTE_P | PTE_W | PTE_U,
+            NULL);
+        assign_physical_page(user_base + i * PAGE_SIZE,p->id);assign_physical_page(user_base + PROC_SIZE - (i + 1) * PAGE_SIZE,p->id);
         memset((void *)(user_base + PROC_SIZE - (i + 1) * PAGE_SIZE), 0, PAGE_SIZE);
     }
 
@@ -78,17 +80,17 @@ void load_process(proc* p, int program) {
 
 void init_processes(){
     memset((void*) procs, 0, sizeof(procs));
-    for(int i = 0; i <= PROC_NUMBER; i++){
+    for(int i = 0; i < PROC_NUMBER; i++){
         procs[i].id = i;
         init_process(&procs[i]);
     }
-    load_process(&procs[PROC_NUMBER],PROC_NUMBER);
-    load_process(&procs[1],1);
+    load_process(&procs[0],0);
+    load_process(&procs[2],2);
 }
 
 void launch_shell(){
-    load_process(&procs[0],0);
-    run(&procs[0]);
+    load_process(&procs[1],1);
+    run(&procs[1]);
 }
 
 void run(proc *p) {
@@ -102,11 +104,15 @@ spinloop:
 }
 
 void schedule(){
-    for(int i = 0; i<PROC_NUMBER; i++){
-        int id = (current->id + 1 + i)%PROC_NUMBER;
+    int id = current->id;
+    for(int i = 0; i<PROC_NUMBER-1; i++){
+        id = (id + 1)%PROC_NUMBER;
+        if(id == 0){
+            id = 1;
+        }
         if(procs[id].state == P_RUNNABLE){
             run(&procs[id]);
         }
     }
-    run(&procs[PROC_NUMBER]);
+    run(&procs[0]);
 }
